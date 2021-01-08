4c21ee6396c39335db9a55f042adfa5c1c8d9c86
gnaneswar0907 <gxg170000@utdallas.edu>
2021-01-08 07:43:01 -0600

    Minor addition

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 
 export default axios.create({
-  baseURL: "https://cors-anywhere.herokuapp.com/https://jobs.github.com",
+  baseURL:
+    "https://protected-beach-09626.herokuapp.com/https://jobs.github.com",
 })
